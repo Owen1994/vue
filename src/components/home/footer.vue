b09e48e9bd1c986be507c6593138e58ec10596e4
Owen1994 <xupeiwen@live.com>
2017-08-14 09:08:52 +0800

    修改部分footer和icon_nav代码

--- src/components/home/footer.vue
+++ src/components/home/footer.vue
@@ -6,11 +6,11 @@
 				<span class="mui-tab-label">首页</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-contact"><span class="mui-badge">9</span></span>
+				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"></span>
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-map">
